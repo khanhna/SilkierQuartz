0bf59008135d8df334cfbcdf34eee07b7c67cc37
khanhna <vn00691699@gmail.com>
2021-02-23 22:25:59 +0700

    first commit login feature

---
+++ src/SilkierQuartz/Controllers/AuthenticateController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SilkierQuartz.Models;
+
+namespace SilkierQuartz.Controllers
+{
+    [AllowAnonymous]
+    public class AuthenticateController : PageControllerBase
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View(new AuthenticateViewModel()
+            {
+                UserName = "this is test",
+                Password = "this is password"
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Login(AuthenticateViewModel request)
+        {
+            Console.WriteLine(request.UserName);
+            return View(request);
+        }
+    }
+}
---
+++ src/SilkierQuartz/Models/AuthenticateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SilkierQuartz.Models
+{
+    public class AuthenticateViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public bool IsPersist { get; set; }
+    }
+}
